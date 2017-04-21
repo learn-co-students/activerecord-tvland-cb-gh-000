fd9aaee372b2b99ef616c4497f97a8b5bc5cc105
Sinan Comert <sinancomert@std.sehir.edu.tr>
2017-04-21 09:40:30 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    r = ""
+    self.characters.each do |t|
+      r += t.name + " - " + t.show.name+" "
+    end
+    r
+  end
+
+
 end
