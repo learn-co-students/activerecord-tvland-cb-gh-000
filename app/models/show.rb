5a4457e36583f63504a54abf2a8135eb37735df9
michael bukachi <michaelbukachi@gmail.com>
2017-06-28 08:20:54 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(call_letters:)
+    network = Network.create(call_letters: call_letters)
+    self.network = network
+  end
 end
