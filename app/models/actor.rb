a5893ba6e99d9853b5429ba96bfc6a0a40beb3b1
Hossam Hosny <hossam.hosni.95@gmail.com>
2017-09-09 02:26:01 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,11 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+
+  def list_roles
+    self.characters.collect {|character| "#{character.name} - #{character.show.name}"}
+  end
+
+  def full_name
+    "#{self.first_name } #{self.last_name}"
+  end
 end
---
+++ db/migrate/007_add_genre_and_day_to_shows.rb
@@ -0,0 +1,6 @@
+class AddGenreAndDayToShows < ActiveRecord::Migration
+	def change
+		add_column :shows, :genre, :string
+		add_column :shows, :day, :string
+	end
+end
