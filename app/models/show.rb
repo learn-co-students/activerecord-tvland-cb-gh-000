a4bd830ce2f45f46c9c39084a7b7527ca4422844
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2018-01-02 22:16:00 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def build_network(options = {})
+    self.network = create_network(options)
+  end
 end
