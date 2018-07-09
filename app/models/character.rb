4b74165bec239680fc63506a85fd7ad0130d3efc
Pranav Sharma <ps20maths@gmail.com>
2018-07-09 17:46:13 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,13 @@
 class Character < ActiveRecord::Base
   
+ belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    "#{name} always says: #{catchphrase}"
+  end
+  def build_show(option = {})
+    show = Show.create(option)
+    self.show = show
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
   
+has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  def build_network(option={})
+    self.network = Network.create(option)
+  end
 end
