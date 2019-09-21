80ed9d70d7281378a9bba78444f77e9d0d90c743
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-21 12:47:43 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    characters.collect {|character| character.actor.full_name}
+  end
+
+  def build_network(hash={})
+    self.network = Network.new(hash)
+  end
+
 end
